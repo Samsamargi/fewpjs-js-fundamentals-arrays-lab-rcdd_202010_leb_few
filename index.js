79dfa0ba735987425b2b7b2fff99772dfa91555a
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-26 09:50:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your solution here!
 const  array =["Milo", "Otis", "Garfield"];
 
+const append =["Milo", "Otis", "Garfield"];
+const prependm =["Milo", "Otis", "Garfield"];
+const removeLast =["Milo", "Otis", "Garfield"];
+const removeFirst =["Milo", "Otis", "Garfield"];
+
+
 
 array.append('Odie');
 array.prepend('');
