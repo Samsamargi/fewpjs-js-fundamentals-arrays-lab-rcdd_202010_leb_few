36086ce8b92cebaf3835ec3b35ed4e052d00e8a6
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-26 09:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-const  array =["Milo", "Otis", "Garfield"];
+]
 
 const append =["Milo", "Otis", "Garfield"];
 const prependm =["Milo", "Otis", "Garfield"];
@@ -8,7 +8,4 @@ const removeFirst =["Milo", "Otis", "Garfield"];
 
 
 
-append.append('Odie');
-array.prepend('Odie');
-array.removeLast;
-array.removeFirst;
+append('Odie');
