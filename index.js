f6bd80d5e15d05aafa9b39ef940570e31f1b5d8d
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-26 09:53:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 const append =["Milo", "Otis", "Garfield"];
-const prependm =["Milo", "Otis", "Garfield"];
+const prepend =["Milo", "Otis", "Garfield"];
 const removeLast =["Milo", "Otis", "Garfield"];
 const removeFirst =["Milo", "Otis", "Garfield"];
 
